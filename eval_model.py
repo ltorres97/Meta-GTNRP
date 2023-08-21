1667605eb6871a4fba99fae232e75a46fbea235d
Luis Torres <luistorres97@hotmail.com>
2023-08-21 21:56:06 +0100

    First commit

---
+++ eval_model.py
@@ -0,0 +1,77 @@
+import torch
+from gnntr_eval import GNNTR_eval
+import statistics
+
+def save_ckp(state, is_best, checkpoint_dir, filename):
+    
+    f_path = checkpoint_dir + filename
+    torch.save(state, f_path)
+
+def save_result(epoch, N, exp, filename):
+            
+    file = open(filename, "a")
+    
+    if epoch < N:
+        file.write("Results: " + "\t")
+        file.write(str(exp) + "\t")
+    if epoch == N:
+        file.write("Results: " + "\t")
+        file.write(str(exp) + "\n")
+        file.write(str(epoch) + " Support Sets: (Mean:"+ str(statistics.mean(exp)) +", SD:" +str(statistics.stdev(exp)) + ") | \t")
+    file.write("\n")
+    file.close()
+
+dataset = "nura"
+gnn = "gin" #gin, graphsage, gcn
+support_set = 10
+pretrained = "pre-trained/supervised_contextpred.pth"
+baseline = 0
+device = "cuda:0"
+
+# Meta-GTNRP - Few-shot GNN-TR architecture
+# GIN
+# GraphSAGE - Graph Isomorphism Network
+# GraphSAGE - Standard Graph Convolutional Network
+
+device = "cuda:0"      
+model_eval = GNNTR_eval(dataset, gnn, support_set, pretrained, baseline)
+
+print("Dataset:", dataset)
+
+roc_auc_list = []
+
+if dataset == "nura":
+    roc = []
+    f1s = []
+    prs = []
+    sns = []
+    sps = []
+    acc = []
+    bacc = []
+    labels =  ['BIN_PR']
+    
+N = 30
+   
+for epoch in range(1, 10000):
+    
+    [roc_scores, f1_scores, p_scores, sn_scores, sp_scores, acc_scores, bacc_scores], gnn_model, transformer_model, gnn_opt, t_opt = model_eval.meta_evaluate()
+
+    if epoch <= N:
+      roc.append(round(roc_scores,4))
+      f1s.append(round(f1_scores,4))
+      prs.append(round(p_scores,4))
+      sns.append(round(sn_scores,4))
+      sps.append(round(sp_scores,4))
+      acc.append(round(acc_scores,4))
+      bacc.append(round(bacc_scores,4))
+    
+    #save_result(epoch, N, roc, "results-exp/ANT_PPARG/mean_GraphSAGE/roc-GraphSAGE_5.txt")
+    #save_result(epoch, N, f1s, "results-exp/ANT_PPARG/mean_GraphSAGE/f1s-GraphSAGE_5.txt")
+    #save_result(epoch, N, prs, "results-exp/ANT_PPARG/mean_GraphSAGE/prs-GraphSAGE_5.txt")
+    #save_result(epoch, N, sns, "results-exp/ANT_PPARG/mean_GraphSAGE/sns-GraphSAGE_5.txt")
+    #save_result(epoch, N, sps, "results-exp/ANT_PPARG/mean_GraphSAGE/sps-GraphSAGE_5.txt")
+    #save_result(epoch, N, acc, "results-exp/ANT_PPARG/mean_GraphSAGE/acc-GraphSAGE_5.txt")
+    #save_result(epoch, N, bacc, "results-exp/ANT_PPARG/mean_GraphSAGE/bacc-GraphSAGE_5.txt")
+
+
+    
---
+++ train_model.py
@@ -0,0 +1,97 @@
+import torch
+from gnntr_train import GNNTR
+
+def save_ckp(state, checkpoint_dir, filename, best):
+    
+    if best == True:
+        f_path = checkpoint_dir + filename
+        torch.save(state, f_path)
+
+dataset = "nura"
+gnn = "gin" #gin, graphsage, gcn
+support_set = 10
+pretrained = "pre-trained/supervised_contextpred.pth"
+baseline = 0
+device = "cuda:0"
+model = GNNTR(dataset, gnn, support_set, pretrained, baseline)
+
+if dataset == "nura":
+    roc = [0]
+    f1s = [0]
+    prs = [0]
+    sns = [0]
+    sps = [0]
+    acc = [0]
+    bacc = [0]
+
+    labels =  ['BIN_PR']
+    
+for epoch in range(1, 251):
+    
+    best = False
+
+    model.meta_train()
+    
+    [roc_scores, f1_scores, p_scores, sn_scores, sp_scores, acc_scores, bacc_scores], gnn_model, transformer_model, gnn_opt, t_opt = model.meta_test() 
+    
+    print(roc_scores, f1_scores, p_scores, sn_scores, sp_scores, acc_scores, bacc_scores)
+
+    checkpoint_gnn = {
+            'epoch': epoch + 1,
+            'state_dict': gnn_model,
+            'optimizer': gnn_opt
+    }
+    
+    checkpoint_transformer = {
+            'epoch': epoch + 1,
+            'state_dict': transformer_model,
+            'optimizer': t_opt
+    }
+    
+    checkpoint_dir = 'checkpoints/meta-gtnrp/BIN_PR/'
+
+    for i in range(0, len(roc_scores)):
+        if roc[i] < roc_scores[i]:
+            roc[i] = roc_scores[i]
+            best = True
+    
+    for i in range(0, len(f1_scores)):
+        if f1s[i] < f1_scores[i]:
+            f1s[i] = f1_scores[i]
+    
+    for i in range(0, len(p_scores)):
+        if prs[i] < p_scores[i]:
+            prs[i] = p_scores[i]
+
+    for i in range(0, len(sn_scores)):
+        if sns[i] < sn_scores[i]:
+            sns[i] = sn_scores[i]
+    
+    for i in range(0, len(sp_scores)):
+        if sps[i] < sp_scores[i]:
+            sps[i] = sp_scores[i]
+
+    for i in range(0, len(acc_scores)):
+        if acc[i] < acc_scores[i]:
+            acc[i] = acc_scores[i]
+    
+    for i in range(0, len(bacc_scores)):
+        if bacc[i] < bacc_scores[i]:
+            bacc[i] = bacc_scores[i]
+    
+    filename = "results-exp/BIN_PR/Meta-GTNRP-10.txt"
+    file = open(filename, "a")
+    file.write("[ROC-AUC, F1-score, Precision, Sensitivity, Specificity, Accuracy, Balanced Accuracy]:\t")
+    file.write("[" + str(roc_scores) + "," + str(f1_scores) + "," + str(p_scores) + "," + str(sn_scores) + "," + str(sp_scores) + "," + str(acc_scores) + "," + str(bacc_scores) + "]" + "\t ; ")
+    file.write("[" + str(roc) + "," +  str(f1s) + "," + str(prs) + "," + str(sns) + "," + str(sps) + "," + str(acc) + "," + str(bacc) + "]") 
+    file.write("\n")
+    file.close()
+    
+    if baseline == 0:
+      save_ckp(checkpoint_gnn, checkpoint_dir, "Meta-GTNRP_GNN_10.pt", best)
+      save_ckp(checkpoint_transformer, checkpoint_dir, "Meta-GTNRP_Transformer_10.pt", best)
+
+    if baseline == 1:
+       save_ckp(checkpoint_gnn, checkpoint_dir, "GIN_GNN_10.pt", best)
+        
+   
